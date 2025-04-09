76b4ff1f02241e89c606f5cd3df8733eb766b16f
sar9494 <batbayarsaruul0@gmail.com>
2025-04-09 18:01:17 +0800

    sadf

--- buy-me-coffee-backend/src/controllers/authenticationControllers/signin.ts
+++ buy-me-coffee-backend/src/controllers/authenticationControllers/signin.ts
@@ -1,5 +1,17 @@
-export const signin = (req: Request, res: Response) => {
-  const { username, password, email } = req.body;
+import prisma from "../../prismaClient";
+
+type User = {
+  username: string;
+  password: string;
+  email: string;
+};
+
+export const signin = async (req: Request, res: Response) => {
+  const { username, password, email } = req.body as unknown as User;
   try {
-  } catch (error) {}
+    const users = await prisma.user.findMany();
+  } catch (error) {
+  } finally {
+    // prisma.$disconnect();
+  }
 };
--- buy-me-coffee-backend/src/index.ts
+++ buy-me-coffee-backend/src/index.ts
@@ -1,17 +1,11 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-async function main() {
-  // ... you will write your Prisma Client queries here
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+import express from "express";
+import cors from "cors";
+import { userRouter } from "./routes/userRouter";
+const app = express();
+const port = 4000;
+app.use(express.json());
+app.use(cors());
+app.use("/", userRouter);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
