b4a129223a35b96faec00f35051ba4d996365ca2
zoezulaa <zoezulaa@gmail.com>
2025-04-16 18:31:48 +0800

    g

--- buy-me-coffee-client/src/app/(auth)/logIn/page.tsx
+++ buy-me-coffee-client/src/app/(auth)/logIn/page.tsx
@@ -5,6 +5,7 @@ import * as Yup from "yup";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import axios from "axios";
+import Link from "next/link";
 
 const LoginSchema = Yup.object().shape({
   email: Yup.string().email("Invalid email").required("Email is required"),
@@ -22,55 +23,64 @@ const handleLogin = async (values: { email: string; password: string }) => {
 };
 export default function LoginPage() {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen">
-      <h2 className="text-2xl font-bold mb-6 text-center">Welcome back</h2>
+    <>
+      <div className="flex items-end justify-end p-4">
+        <Link href={"/signUp"}>
+          <Button className="p-7 rounded-full bg-white text-black hover:bg-amber-200 font-bold text-lg">
+            Sign Up
+          </Button>
+        </Link>
+      </div>
+      <div className="flex flex-col items-center justify-center w-full h-screen">
+        <h2 className="text-2xl font-bold mb-6 text-center">Welcome back</h2>
 
-      <Formik
-        initialValues={{ email: "", password: "" }}
-        validationSchema={LoginSchema}
-        onSubmit={values => {
-          handleLogin(values);
-        }}>
-        {({ isSubmitting }) => (
-          <Form className="space-y-4 w-full flex flex-col items-center justify-center">
-            <div className="w-1/3 ">
-              <h1 className="text-sm text-forgeground font-bold">Email</h1>
-              <Field
-                as={Input}
-                type="email"
-                className="w-full"
-                name="email"
-                placeholder="Email"
-              />
-              <ErrorMessage
-                name="email"
-                component="div"
-                className="text-sm text-red-500 mt-1"
-              />
-            </div>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={LoginSchema}
+          onSubmit={values => {
+            handleLogin(values);
+          }}>
+          {({ isSubmitting }) => (
+            <Form className="space-y-4 w-full flex flex-col items-center justify-center">
+              <div className="w-1/3 ">
+                <h1 className="text-sm text-forgeground font-bold">Email</h1>
+                <Field
+                  as={Input}
+                  type="email"
+                  className="w-full"
+                  name="email"
+                  placeholder="Email"
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className="text-sm text-red-500 mt-1"
+                />
+              </div>
 
-            <div className="w-1/3">
-              <h2 className="text-sm text-forgeground font-bold">Password</h2>
-              <Field
-                as={Input}
-                type="password"
-                name="password"
-                className="w-full"
-                placeholder="Password"
-              />
-              <ErrorMessage
-                name="password"
-                component="div"
-                className="text-sm text-red-500 mt-1"
-              />
-            </div>
+              <div className="w-1/3">
+                <h2 className="text-sm text-forgeground font-bold">Password</h2>
+                <Field
+                  as={Input}
+                  type="password"
+                  name="password"
+                  className="w-full"
+                  placeholder="Password"
+                />
+                <ErrorMessage
+                  name="password"
+                  component="div"
+                  className="text-sm text-red-500 mt-1"
+                />
+              </div>
 
-            <Button type="submit" disabled={isSubmitting} className="w-1/3">
-              {isSubmitting ? "Logging in..." : "Continue"}
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </div>
+              <Button type="submit" disabled={isSubmitting} className="w-1/3">
+                {isSubmitting ? "Logging in..." : "Continue"}
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </>
   );
 }
--- buy-me-coffee-client/src/app/(auth)/signUp/page.tsx
+++ buy-me-coffee-client/src/app/(auth)/signUp/page.tsx
@@ -3,12 +3,21 @@
 import { useState } from "react";
 import { Step1 } from "./_components/Step1";
 import { Step2 } from "./_components/Step2";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function SignUpStepperForm() {
   const [step, setStep] = useState<number>(0);
 
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-white flex-col">
+    <div className="w-full h-screen flex items-center justify-center relative bg-white flex-col">
+      <div className="flex items-end justify-end p-4 top-0 right-0 absolute">
+        <Link href={"/logIn"}>
+          <Button className="p-7 rounded-full bg-white text-black hover:bg-amber-200 font-bold text-lg">
+            Log
+          </Button>
+        </Link>
+      </div>
       <div className="w-1/3 py-10">
         <h2 className="text-xl font-bold">Create Your Account</h2>
         <p className="text-sm text-muted-foreground">
