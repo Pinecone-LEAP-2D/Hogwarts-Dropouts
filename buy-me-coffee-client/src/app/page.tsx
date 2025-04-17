20e9d8bdbda44aa45a367c6ed07ee63cc9d584d9
sar9494 <batbayarsaruul0@gmail.com>
2025-04-17 16:02:35 +0800

    asdf

--- buy-me-coffee-client/src/app/page.tsx
+++ buy-me-coffee-client/src/app/page.tsx
@@ -1,23 +1,33 @@
 "use client";
 import { Header } from "@/components/Header";
-import { Button } from "@/components/ui/button";
+import { SwitchPageButton } from "./(auth)/signUp/_components/SwitchPageButton";
 export default function Home() {
   return (
     <div className="w-full h-screen flex flex-col justify-between relative">
       <Header />
       <div className="flex justify-center h-full items-center flex-col gap-8">
-        <img src="coffee.png" alt="" className="w-[350px] h-[350px]" />
+        <img
+          src="https://res.cloudinary.com/dszot6j60/image/upload/v1744874396/coffee_tetumj.png"
+          alt=""
+          className="w-[350px] h-[350px]"
+        />
         <div>
-          <Button className="p-10 px-20 rounded-full bg-amber-300 text-black hover:bg-amber-200 font-bold text-xl">
-            Start my page
-          </Button>
+          <SwitchPageButton name={"Start my page"} link="signUp" />
         </div>
       </div>
       <div className="flex justify-end items-end absolute bottom-0.5 right-0">
-        <img src="coffeegang.png" alt="" className="w-[400px] h-[400px] " />
+        <img
+          src="https://res.cloudinary.com/dszot6j60/image/upload/v1744874578/coffeegang_ylxuqy.png"
+          alt=""
+          className="w-[400px] h-[400px] "
+        />
       </div>
       <div className="flex justify-start items-start absolute bottom-1 p-10">
-        <img src="cup.png" alt="" className="w-[250px] h-[250px]" />
+        <img
+          src="https://res.cloudinary.com/dszot6j60/image/upload/v1744874635/cup_n7qzxb.png"
+          alt=""
+          className="w-[250px] h-[250px]"
+        />
       </div>
     </div>
   );
--- buy-me-coffee-client/src/components/Header.tsx
+++ buy-me-coffee-client/src/components/Header.tsx
@@ -20,30 +20,23 @@ import { Button } from "./ui/button";
 // };
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { SwitchPageButton } from "@/app/(auth)/signUp/_components/SwitchPageButton";
 
 export const Header = () => {
   const router = useRouter();
 
   return (
     <div className="flex p-5 justify-between px-15 w-full">
-      <div
-        // onClick={() => router.push("/home")}
-        className="flex font-extrabold items-center gap-2 hover:opacity-80 transition cursor-pointer">
-        <Link href="/" className="cursor-pointer">
-          <a>
-            <Coffee size={55} />
-            <p className="text-xl">Buy Me Coffee</p>
-          </a>
-        </Link>
-      </div>
+      <Link href="/" className="cursor-pointer">
+        <div className="flex font-extrabold items-center gap-2 hover:opacity-80 transition cursor-pointer">
+          <Coffee size={55} />
+          <p className="text-xl">Buy Me Coffee</p>
+        </div>
+      </Link>
 
       <div className="flex gap-2">
-        <Button className="p-7 rounded-full bg-amber-300 text-black hover:bg-amber-200 font-bold text-lg">
-          Log in
-        </Button>
-        <Button className="p-7 rounded-full bg-white border-amber-300 border text-black hover:bg-amber-200 font-bold text-lg">
-          Sign Up
-        </Button>
+        <SwitchPageButton name={"Log in"} link="logIn" />
+        <SwitchPageButton name={"Sign up"} link="signUp" />
       </div>
     </div>
   );
